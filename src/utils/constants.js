354273b7234ea2482e2bd315a28848ca5bcd1e24
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-10-04 03:46:29 +0300

    промежуточный

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,9 @@ const URL_PROTOCOL = 'http';
 // const MAIN_DOMAIN = 'api.lerush.nomoredomains.sbs';
 const MAIN_DOMAIN = 'localhost:3000';
 const MAIN_API = `${URL_PROTOCOL}://${MAIN_DOMAIN}`;
-const MOVIES_API = 'https://api.nomoreparties.co/beatfilm-movies';
 
-export { MAIN_API, MOVIES_API };
+const MOVIES_DOMAIN = 'api.nomoreparties.co';
+const MOVIES_API = `https://${MOVIES_DOMAIN}/beatfilm-movies`;
+const MOVIES_IMAGES = `https://${MOVIES_DOMAIN}`;
+
+export { MAIN_API, MOVIES_API, MOVIES_IMAGES };
