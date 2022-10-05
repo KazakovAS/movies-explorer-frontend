7ae96f8f5c656c772433f3d3c1aa43c0f1c0c1f4
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-10-05 21:13:26 +0300

    some

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,15 +1,10 @@
-import { useContext } from 'react';
 import { Route, Redirect } from "react-router-dom";
 
-import { AuthContext } from "../../contexts/AuthContext";
-
 const ProtectedRoute = ({ component: Component, ...props }) => {
-  const { loggedIn } = useContext(AuthContext);
-
   return (
     <Route>
       {() =>
-        loggedIn ? <Component {...props} /> : <Redirect to="./signin" />
+        (localStorage.getItem('jwt')) ? <Component {...props} /> : <Redirect to="./signin" />
       }
     </Route>
   );
