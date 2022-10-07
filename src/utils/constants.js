7bcb7392ac07cf1e69479603942e407374d10637
Alexey Kazakov <a.kazakov@betaonline.ru>
2022-10-07 20:22:39 +0300

    some

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // const URL_PROTOCOL = document.location.href.includes('https://') ? 'https' : 'http';
-const URL_PROTOCOL = 'http';
-// const MAIN_DOMAIN = 'api.lerush.nomoredomains.sbs';
-const MAIN_DOMAIN = 'localhost:3000';
+const URL_PROTOCOL = 'https';
+const MAIN_DOMAIN = 'api.lerush.nomoredomains.sbs';
+// const MAIN_DOMAIN = 'localhost:3000';
 const MAIN_API = `${URL_PROTOCOL}://${MAIN_DOMAIN}`;
 
 const MOVIES_DOMAIN = 'api.nomoreparties.co';
